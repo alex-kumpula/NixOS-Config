0ed4bc29ad6c783608489f6c683c614525c2746d
Alex Kumpula <71002475+alex-kumpula@users.noreply.github.com>
2025-05-06 17:15:17 -0600

    update

--- flake.nix
+++ flake.nix
@@ -11,16 +11,21 @@
     };
   };
 
-  outputs = { self, nixpkgs, nur, ... }:
-    let
-      lib = nixpkgs.lib;
-    in {
-      nixosConfigurations = {
-        nixos-tutorial-hostname = lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [ ./configuration.nix ];
-          specialArgs = { inherit inputs outputs; };  # Pass nur as a special argument
-        };
+  outputs = { 
+    self, 
+    nixpkgs, 
+    nur, 
+    ... 
+  } @ inputs: let
+    inherit (self) outputs;
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      nixos-tutorial-hostname = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+        specialArgs = { inherit inputs outputs; };  # Pass nur as a special argument
       };
     };
+  };
 }
